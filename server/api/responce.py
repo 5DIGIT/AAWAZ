695cb0a9d97b7020165a7a29215a5a76d9233fd9
Azzam Husain <63924876+azzamh15@users.noreply.github.com>
2023-03-22 14:11:38 +0530

    Update responce.py

--- server/api/responce.py
+++ server/api/responce.py
@@ -1,7 +1,7 @@
 import requests
 #from .models import Users, Complaints
 
-url = "http://192.168.137.1:5000/emergency/"
+url = ""
 def get_complaints(image,latlong,description):
     # complaint = Complaints.objects.filter(complaint_id=complaint_id)    
     # print(complaint)
